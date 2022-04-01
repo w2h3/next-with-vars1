6457e8b79ab0520e4c8eb845605bc296af74c458
James Wingert <jameswingert1@gmail.com>
2022-03-31 23:11:08 -0700

    Fixed styling

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -13,61 +13,19 @@ export const AboutUs = () => {
 
       <div className="bg-[#f0fdff]">
     
-        <div className="container">
+        <div className="container ">
           {/* px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20 */}
-          <div className="grid grid-cols-1  lg:grid-cols-2">
-            <div className="flex flex-col justify-center md:pr-8 xl:pr-0 lg:max-w-">
-              <div className="flex items-center justify-center w-16 h-16 mb-4 rounded-full ">
-                {/* <svg className="text-teal-900 w-7 h-7" viewBox="0 0 24 24">
-                <polyline
-                  fill="none"
-                  stroke="currentColor"
-                  strokeWidth="2"
-                  strokeLinecap="round"
-                  strokeMiterlimit="10"
-                  points=" 8,5 8,1 16,1 16,5"
-                  strokeLinejoin="round"
-                />
-                <polyline
-                  fill="none"
-                  stroke="currentColor"
-                  strokeWidth="2"
-                  strokeLinecap="round"
-                  strokeMiterlimit="10"
-                  points="9,15 1,15 1,5 23,5 23,15 15,15"
-                  strokeLinejoin="round"
-                />
-                <polyline
-                  fill="none"
-                  stroke="currentColor"
-                  strokeWidth="2"
-                  strokeLinecap="round"
-                  strokeMiterlimit="10"
-                  points="22,18 22,23 2,23 2,18"
-                  strokeLinejoin="round"
-                />
-                <rect
-                  x="9"
-                  y="13"
-                  fill="none"
-                  stroke="currentColor"
-                  strokeWidth="2"
-                  strokeLinecap="round"
-                  strokeMiterlimit="10"
-                  width="6"
-                  height="4"
-                  strokeLinejoin="round"
-                />
-              </svg> */}
-              </div>
-              <div className="max-w-xl mb-6">
-                <h2 className="max-w-lg mb-6 font-sans text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl sm:leading-none">
+          <div className="grid grid-cols-1  md:grid-cols-2 ">
+            <div className="flex flex-col   lg:max-w-6xl">
+
+
+                <h2 className=" mb-6 font-sans text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl sm:leading-none text-center md:text-left  ">
                   Welcome to
                   <br />
                   <p className="block text-[#7ba009]">Neurology and Sleep </p>
                   <span className="inline-block ">Medicine Associates</span>
                 </h2>
-                <p className="text-base text-gray-700 md:text-lg">
+                <p className="text-base text-gray-700 md:text-lg md:mr-20 mb-10">
                   As a neurologic and sleep disorders specialty clinic,
                   Neurology & Sleep Medicine Associates’ work is in a unique
                   partnership of health care: providing and interpreting your
@@ -77,9 +35,8 @@ export const AboutUs = () => {
                   been carefully structured to provide a personal approach to
                   patient care. Our goal is to provide you with very good
                   service and pay close attention to your individual needs and
-                  concerns. Some of our core tenants are:
+                  concerns. 
                 </p>
-              </div>
             </div>
             <div className="">
             {/* <div className="flex items-center justify-center -mx-4 lg:pl-8"> */}
