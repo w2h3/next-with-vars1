9680ebcd3d4b2469ed7224ed24f31aa236d969dc
James Wingert <jameswingert1@gmail.com>
2022-04-29 01:20:36 -0700

    Stopped tracking env.local, and env.staging

--- src/components/layout/Banner.tsx
+++ src/components/layout/Banner.tsx
@@ -15,7 +15,7 @@ export default function Banner({ pageName, imageURL }) {
             layout="fill"
             priority
             objectFit="cover"
-            quality={80}
+            quality={60}
             // width={103}
             // height={70}
           />
