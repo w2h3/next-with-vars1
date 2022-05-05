647ea5b742eb819457a15159fb09e11c605dcd9c
James Wingert <jameswingert1@gmail.com>
2022-05-04 20:45:09 -0700

    added new next image

--- next.config.js
+++ next.config.js
@@ -7,7 +7,7 @@ module.exports = {
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     nextImageExportOptimizer: {
-      imageFolderPath: 'public/images',
+      imageFolderPath: 'public',
       exportFolderPath: 'out',
       quality: 75,
     },
