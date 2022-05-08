0b7ca0bc1b167f6d3f9a24645c501041a3a403d2
James Wingert <jameswingert1@gmail.com>
2022-05-08 13:48:10 -0700

    fixed console errors

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,31 @@
 import Head from 'next/head';
+import Script from 'next/script';
 
 import '../styles/globals.css';
 import '../components/swiper/swiper.css';
 
 import Layout from '../components/layout/Layout';
 function MyApp({ Component, pageProps }) {
-
   return (
     <>
+      <Script
+        strategy='lazyOnload'
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      />
+      <Script
+        id='google-analytics-script'
+        strategy='lazyOnload'
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
       <Head>
         <title>Neurology & Sleep Medicine Associates</title>
         <link rel='icon' type='image/icon' sizes='16x16' href='/favicon.ico' />
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -15,26 +15,7 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang='en'>
-        <Head>
-          {' '}
-          {/* Global Site Tag (gtag.js) - Google Analytics */}
-          <script
-            async
-            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
-          />
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
-              page_path: window.location.pathname,
-            });
-          `,
-            }}
-          />
-        </Head>
+        <Head />
         <body>
           <Main />
           <NextScript />
