a5b55dcf4d5dfcd3e9490d9980aab70b13e4b32f
James Wingert <jameswingert1@gmail.com>
2022-05-08 15:08:29 -0700

    fixed SEO

--- src/pages/cs.tsx
+++ src/pages/cs.tsx
@@ -1,29 +1,27 @@
+import Link from 'next/link';
 import * as React from 'react';
 
 import Seo from '@/components/Seo';
 
-export default function ComingSoon() {
+export default function cs() {
   return (
     <>
       <Seo />
 
-      <main>
-        <section className='bg-white'>
-          <div className='layout flex min-h-screen flex-col items-center justify-center text-center text-black'>
-            <h1 className='mt-8 text-4xl md:text-6xl'>
-              This feature will be completed by 5/13/22. Please download and
-              fillout the offline form for now. Thank you for your
-              understanding.
-            </h1>
-            <link
-              className='mt-4 text-blue-600 underline underline-offset-2 md:text-lg'
-              href='/patients'
-            >
-              Back to Patients Page.
-            </link>
-          </div>
-        </section>
-      </main>
+      <section className='container'>
+        <div className='layout flex min-h-screen flex-col items-center justify-center text-center text-black'>
+          <h1 className='mt-8 text-4xl md:text-6xl'>
+            This feature will be completed by 5/13/22. Please download and
+            fillout the offline form for now. Thank you for your understanding.
+          </h1>
+          <Link
+            className='mt-4 text-blue-600 underline underline-offset-2 md:text-lg'
+            href='/patients'
+          >
+            Back to Patients Page.
+          </Link>
+        </div>
+      </section>
     </>
   );
 }
