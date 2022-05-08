f5ae3b060c622ed3acd352a5b96f4c73422d9699
James Wingert <jameswingert1@gmail.com>
2022-05-08 15:11:28 -0700

    fixed SEO

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -10,13 +10,15 @@ export default function notfoundpage() {
 
       <section className='container'>
         <div className='layout flex min-h-screen flex-col items-center justify-center text-center text-black'>
-          <h1 className='mt-8 text-4xl md:text-6xl'>Page Not Found</h1>
-          <Link
-            className='mt-4 text-blue-600 underline underline-offset-2 md:text-lg'
-            href='/'
-          >
-            Back to Home
-          </Link>
+          <h1 className='mt-8 text-4xl md:text-6xl'>Page Not Found</h1>{' '}
+          <div className='mt-4 text-blue-600 underline underline-offset-2 md:text-lg'>
+            <Link
+              className='mt-4 text-blue-600 underline underline-offset-2 md:text-lg'
+              href='/'
+            >
+              Back to Home
+            </Link>
+          </div>
         </div>
       </section>
     </>
--- src/pages/cs.tsx
+++ src/pages/cs.tsx
@@ -10,16 +10,15 @@ export default function cs() {
 
       <section className='container'>
         <div className='layout flex min-h-screen flex-col items-center justify-center text-center text-black'>
-          <h1 className='mt-8 text-4xl md:text-6xl'>
+          <h1 className='mt-8 text-3xl'>
             This feature will be completed by 5/13/22. Please download and
             fillout the offline form for now. Thank you for your understanding.
           </h1>
-          <Link
-            className='mt-4 text-blue-600 underline underline-offset-2 md:text-lg'
-            href='/patients'
-          >
-            Back to Patients Page.
-          </Link>
+          <div className='mt-4 text-blue-600 underline underline-offset-2 md:text-lg'>
+            <Link className='' href='/patients'>
+              Back to Patients Page.
+            </Link>
+          </div>
         </div>
       </section>
     </>
