a4a69393928a3dfbeaa18274d92f0c8583c6f181
James Wingert <jameswingert1@gmail.com>
2022-05-12 18:37:25 -0700

    added patient form

--- src/pages/cs.tsx
+++ src/pages/cs.tsx
@@ -11,8 +11,8 @@ export default function cs() {
       <section className='container'>
         <div className='layout flex min-h-screen flex-col items-center justify-center text-center text-black'>
           <h1 className='mt-8 text-3xl'>
-            Thank you, your form as been submitted successfully! We look forward
-            to seeing you.
+            Thank you, your form has been submitted successfully! We look
+            forward to seeing you.
           </h1>
           <div className='mt-4 text-blue-600 underline underline-offset-2 md:text-lg'>
             <Link href='/'>Back to home.</Link>
