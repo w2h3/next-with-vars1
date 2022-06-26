a7afcbd0987b80eee9e788407154b57ec3a6b4f1
James Wingert <jameswingert1@gmail.com>
2022-06-26 01:48:39 -0700

    Removed unneccessary files

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,49 +1,13 @@
 import Head from 'next/head';
-import { useRouter } from 'next/router';
-import Script from 'next/script';
-import { useEffect } from 'react';
 
 import '../styles/globals.css';
 import '../components/swiper/swiper.css';
 
 import Layout from '../components/layout/Layout';
-import * as gtag from '../components/lib/gtag';
 
 function MyApp({ Component, pageProps }) {
-  const router = useRouter();
-  useEffect(() => {
-    const handleRouteChange = (url) => {
-      gtag.pageview(url);
-    };
-    router.events.on('routeChangeComplete', handleRouteChange);
-    router.events.on('hashChangeComplete', handleRouteChange);
-    return () => {
-      router.events.off('routeChangeComplete', handleRouteChange);
-      router.events.off('hashChangeComplete', handleRouteChange);
-    };
-  }, [router.events]);
-
   return (
     <>
-      {/* Global Site Tag (gtag.js) - Google Analytics */}
-      <Script
-        strategy='afterInteractive'
-        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
-      />
-      <Script
-        id='gtag-init'
-        strategy='afterInteractive'
-        dangerouslySetInnerHTML={{
-          __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${gtag.GA_TRACKING_ID}', {
-              page_path: window.location.pathname,
-            });
-          `,
-        }}
-      />
       <Head>
         <title>Neurology & Sleep Medicine Associates</title>
 
